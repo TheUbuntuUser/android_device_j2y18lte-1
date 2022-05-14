74a9720c371cf6e9d790c8d567e53622cf919048
TheUbuntuUser <birthday@hints.or.kr>
2022-05-14 15:39:25 +0900

    init arm64 and some update

--- aosp_j2y18lte.mk
+++ aosp_j2y18lte.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common DU stuff
@@ -30,7 +31,7 @@ PRODUCT_DEVICE := j2y18lte
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_NAME := aosp_j2y18lte
 PRODUCT_MODEL := Galaxy J2 Pro
-TARGET_GAPPS_ARCH := arm
+TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 TARGET_VENDOR := samsung
